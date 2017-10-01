8a3bb9e081f3acadc924195ee50d0e62671ae0c3
SnugglinBunny <defiantnarwhal@gmail.com>
2017-10-01 15:16:22 +0100

    Draw Triangle, couldn't get shaders to work yet.

--- COMP220/COMP220_Examples/01_OpenGL/main.cpp
+++ COMP220/COMP220_Examples/01_OpenGL/main.cpp
@@ -1,4 +1,4 @@
-//main.cpp - defines the entry point of the application
+///main.cpp - defines the entry point of the application
 
 #include "main.h"
 
@@ -57,6 +57,27 @@ int main(int argc, char* args[])
 		return 1;
 	}
 
+	//Create Vertex Array for the triangle
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// An array of 3 vectors which represents 3 vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+	};
+
+	// This will identify our vertex buffer
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
 	//Event loop, we will loop until running is set to false, usually if escape has been pressed or window is closed
 	bool running = true;
 	float RED = 255;
@@ -102,6 +123,21 @@ int main(int argc, char* args[])
 		glClearColor(RED, GREEN, BLUE, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+
 		SDL_GL_SwapWindow(window);
 
 	}
@@ -109,7 +145,8 @@ int main(int argc, char* args[])
 	//Delete Context
 	SDL_GL_DeleteContext(gl_Context);
 
-
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	//Destroy the window and quit SDL2, NB we should do this after all cleanup in this order!!!
 	//https://wiki.libsdl.org/SDL_DestroyWindow
